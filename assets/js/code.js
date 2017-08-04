2f825d8cbae02972dc478bba585c0986b2f14f9e
Thiago Oliveira <tmeloliveira@gmail.com>
2017-08-03 21:33:17 -0300

    Initial commit for be parametric website

---
+++ assets/js/code.js
@@ -0,0 +1,21 @@
+(function() {
+  var combinations = [
+    [],
+    [],
+    [],
+    [],
+  ];
+
+  var typedTitle = new Typed('.title__typing', {
+   strings: ['ousado', 'bacana', 'danada', '<strong>parametric</strong>'],
+   startDelay: 100,
+   backDelay: 2000,
+   typeSpeed: 60,
+   backSpeed: 0,
+   smartBackspace: true,
+   loop: false,
+   showCursor: false
+ });
+
+
+})()
