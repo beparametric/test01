4f8e56fdc298b2a8831fc206809e557905e4151f
Thiago Oliveira <tmeloliveira@gmail.com>
2017-08-03 21:54:32 -0300

    Add combinantion of words

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,21 +1,21 @@
 (function() {
   var combinations = [
-    [],
-    [],
-    [],
-    [],
+    ['innovative', 'smart', 'ousado', 'curious', 'unique', '<strong>parametric</strong>'],
+    ['creative', 'poetic', 'simple', 'complex', 'selvagem', '<strong>parametric</strong>'],
+    ['different', 'bacana', 'cool', 'flexible', 'more', '<strong>parametric</strong>'],
+    ['all', 'organized', 'chaotic', 'full', '@#$%&*!', '<strong>parametric</strong>'],
   ];
 
+  randomIndex = Math.floor(Math.random()*(4));
+
   var typedTitle = new Typed('.title__typing', {
-   strings: ['ousado', 'bacana', 'danada', '<strong>parametric</strong>'],
+   strings: combinations[randomIndex],
    startDelay: 100,
-   backDelay: 2000,
+   backDelay: 500,
    typeSpeed: 60,
    backSpeed: 0,
    smartBackspace: true,
    loop: false,
    showCursor: false
  });
-
-
-})()
+})();
